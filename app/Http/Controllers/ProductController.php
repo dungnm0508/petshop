5eadec64f8ed6b33368493afeac31736bde20b5e
dungnm <dungnm@zotabox.com>
2019-08-27 15:44:41 +0700

    ad

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -28,6 +28,10 @@ class ProductController extends Controller
       }
       public function getProductTest(){
          $products =  Product::all();
+
+
+
+         
          return $products;
       }
 }
